20951a5541773a2d2b01befbdb4b33b90787681e
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-10-01 14:57:42 -0800

    Add files via upload

---
+++ kelvin/cmake-build-debug/CMakeFiles/kelvin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kelvin.dir/main.cpp.obj"
+  "kelvin.exe"
+  "kelvin.exe.manifest"
+  "kelvin.lib"
+  "kelvin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kelvin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
